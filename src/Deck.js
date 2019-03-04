1365f150585249937be3f673e3c17149d4d76148
boyang1020 <boyang1020@gmail.com>
2019-03-03 21:04:39 -0800

    the default props systems

--- src/Deck.js
+++ src/Deck.js
@@ -6,6 +6,10 @@ const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
 const SWIPE_OUT_DURATION = 250;
 
 class Deck extends Component {
+    static defaultProps = {
+        onSwipeRight: () => {}
+    }
+    
     constructor(props) {
         super(props);
 
@@ -42,7 +46,9 @@ class Deck extends Component {
 
     onSwipeComplete(direction) {
         const { onSwipeRight, onSwiprLeft, data } = this.props;
-        const item = data[this.state.index]
+        const item = data[this.state.index];
+
+
 
         direction === 'right' ? onSwipeRight(item) : onSwiprLeft(item);
     }
